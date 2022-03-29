fcb695d4b77ddd286c2f7f31d27c64a612a392e6
PRATHAM GUPTA <PRATHAM17GUPTA@GMAIL.COM>
2022-03-30 00:25:20 +0530

    first commit

---
+++ src/components/products.js
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    title: "Samsung S21",
+    description: "Black",
+    price: "49999",
+    img: "https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    quantity: 1,
+  },
+  {
+    id: 2,
+    title: "Samsung M21",
+    description: "White",
+    price: "24999",
+    img: "https://images.pexels.com/photos/47261/pexels-photo-47261.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    quantity: 1,
+  },
+  {
+    id: 3,
+    title: "Iphone 12",
+    description: "Purple",
+    price: "99999",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hIfcIPyxS._SL1500_.jpg",
+    quantity: 1,
+  },
+  {
+    id: 4,
+    title: "Samsung S21 Pro",
+    description: "Black",
+    price: "59999",
+    img: "https://images.pexels.com/photos/404280/pexels-photo-404280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    quantity: 1,
+  },
+  {
+    id: 5,
+    title: "Iphone 12 Pro",
+    description: "Purple",
+    price: "129999",
+    img: "https://images-na.ssl-images-amazon.com/images/I/71hIfcIPyxS._SL1500_.jpg",
+    quantity: 1,
+  }
+];
